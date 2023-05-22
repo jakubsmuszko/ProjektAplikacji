60f918e250a1d752fd15f8e8e294622f54def392
Dawid Gabriel <dawidgabriel5d@gmail.com>
2023-05-22 19:43:15 +0200

    test

---
+++ app/src/main/java/com/example/preject/CheckOut.kt
@@ -0,0 +1,11 @@
+package com.example.preject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckOut : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_out)
+    }//csdsdc
+}
