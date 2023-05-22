cffe63f4f0f2a2ef4ad4db64c7b5f40f18ddc7be
Jakub Smuszko <54269449+jakubsmuszko@users.noreply.github.com>
2023-05-22 20:04:54 +0200

    Update Menu.kt test
    test

--- app/src/main/java/com/example/preject/Menu.kt
+++ app/src/main/java/com/example/preject/Menu.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class Menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        setContentView(R.layout.activity_menu)//testtest
     }
 }
